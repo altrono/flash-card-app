3eabd1b0264d39402f6c74f1e67a909a3a9d3b15
altrono <hbotema16@gmail.com>
2023-02-09 14:23:46 +0200

    Added codes to save the progress in to a csv file

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-# Convert the csv to DataFrame
-data = pandas.read_csv('data/french_words.csv')
-
-# Convert DataFrame to Python dict Object, then with orient set to 'records' is return a List of dict Object
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    # Convert the csv to DataFrame
+    data = pandas.read_csv('data/words_to_learn.csv')
+except FileNotFoundError:
+    original_date = pandas.read_csv('data/french_words.csv')
+    to_learn = original_date.to_dict(orient='records')
+else:
+    # Convert DataFrame to Python dict Object, then with orient set to 'records' is return a List of dict Object
+    to_learn = data.to_dict(orient="records")
+
 
 def next_card():
     global current_card, flip_timer
@@ -27,6 +33,14 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+def is_known():
+    to_learn.remove(current_card)
+
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+
+    next_card()
 
 window = Tk()
 window.title('Flashy')
@@ -49,7 +63,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=next_ca
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file='images/right.png')
-known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 
